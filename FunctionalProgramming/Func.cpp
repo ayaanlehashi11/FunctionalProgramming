86c7f4f057d9c6f409fa6972ed266912e26b301f
Ayaanle11 <ayaanlemohamed540@gmail.com>
2022-07-10 18:59:32 +0300

    Add project files.

---
+++ FunctionalProgramming/Func.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <functional>
+#include "Func.h"
+
+
+
+ template <typename T , typename U> Func<T , U> ::Func(int x) {
+	std::cout << x << std::endl;
+}
+template <typename T , typename U> T Func<T , U>:: ApplyFunctional(std::function<T()>& func){
+	func1();
+}
+template <typename T, typename U > U Func<T, U> :: ApplyLambda(std::function<U()>& func)
+{
+	func();
+}
---
+++ FunctionalProgramming/Func.h
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <functional>
+#pragma once
+#ifndef Func_h
+#define Func_h
+#endif
+
+/* project - kan waxa uu ka hadlaya functional programming 
+sida u shaqeeyo anigo adeesanaya lambda expression iyo 
+functional pointers oo aad ku adeesan karto C iyo C++ 
+(micnaha lambda expression iyo function pointers waxad ku adeesan karta C++ halka function pointers aad ku adeesan karto C,
+sido kale qaabka loo adeesado functional header iyo functional class-ka
+*/
+
+template <typename U , typename T>
+struct Func {
+public:
+	Func(int x);
+		// qaybtan waxan adeesaday function pointer waxan ugu talagay inan pass gareeyo higher functionka apply
+	void ApplyFuncPointer(void(*FuncName)(int , int), int num1, int num2);
+	//labadan overload mid waxan pass-gareynayna lambda expression
+	T ApplyFunctional(std::function<T()>&func);
+	//kan labaad waxan pass-gareynayna std::function type kaas bind ku sameynaya function-ka aynu u baahano
+	U ApplyLambda(std::function<U()>&func);
+public:
+	int x_;
+};
+
